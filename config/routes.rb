dd99507eb2c3e88eed2b762523468655b1bdbd77
Lucas Castro <lucasmcastro@gmail.com>
2012-10-28 17:19:20 -0300

    Making search and filter actions via get

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Certificados::Application.routes.draw do
   get "lecturers/imports/new" => "lecturers#new_import", :as => :new_lecturers_import
   post "lecturers/imports" => "lecturers#create_import", :as => :lecturers_import
   post "certificates/:certificate_id/mail/" => "certificate_mail#create", :as => :email_certificate
-  post "certificates/search/" => "certificates#search", :as => :search_certificates
+  get "certificates/search/" => "certificates#search", :as => :search_certificates
   get "certificates/" => "certificates#index", :as => :certificates
-  post "certificates/filter" => "certificates#filter", :as => :filter_certificates
+  get "certificates/filter" => "certificates#filter", :as => :filter_certificates
 
   [:lecturers, :students].each do |r|
     resources r do
